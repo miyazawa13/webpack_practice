c93b53dcf7db61c6f935ee1c02bc02d6778a8266
miyazawa <77086655+miyazawa13@users.noreply.github.com>
2022-05-05 14:26:10 +0900

    Build css with sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 
 module.exports = {
     entry: './src/javascripts/main.js',
@@ -9,16 +10,43 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: 'javascripts/main.js',
     },
+    mode: "development",
+    //仮想サーバーの設定
+    devServer: {
+        static: "dist",
+        // contentBase: path.join(__dirname, "dist"), //ルートディレクトリの指定
+        open: true, //ブラウザを自動的に起動
+    },
     module: {
         rules: [
             {
-                test: /\.css/,
+                // 対象となるファイルの拡張子(scssかsassかcss)
+                test: /\.(css|sass|scss)/,
+                // Sassファイルの読み込みとコンパイル
                 use: [
+                    // CSSファイルを抽出するように MiniCssExtractPlugin のローダーを指定
                     {
                         loader: MiniCssExtractPlugin.loader,
                     },
+                    // CSSをバンドルするためのローダー
                     {
                         loader: 'css-loader',
+                    },
+                    // PostCSS（autoprefixer）の設定
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                        // production モードでなければソースマップを有効に
+                        // sourceMap: enabledSourceMap,
+                        postcssOptions: {
+                            // ベンダープレフィックスを自動付与
+                            plugins: [require("autoprefixer")({ grid: true })]
+                        }
+                        }
+                    },
+                    // Sass を CSS へ変換するローダー
+                    {
+                        loader: 'sass-loader',
                     },
                 ],
             },
@@ -56,16 +84,22 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: './stylesheets/main.css',
+            filename: './stylesheets/main.css', // distの中にあるcssフォルダにstyle.cssを出力
         }),
         new HtmlWebpackPlugin({
             template: './src/templates/index.pug',
             filename: 'index.html',
+            alwaysWriteToDisk: true,
         }),
         new HtmlWebpackPlugin({
             template: './src/templates/access.pug',
             filename: 'access.html',
         }),
-        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/templates/members/taro.pug',
+            filename: 'members/taro.html',
+        }),
+        new HtmlWebpackHarddiskPlugin(), // HTMLファイル変更時にホットリロード
+        new CleanWebpackPlugin(), // dist内の不要なファイルやフォルダを消す
     ],
 }
